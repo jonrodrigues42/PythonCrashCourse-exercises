5fa43642bf999aba0d580543e90d6889225319de
Jon Rodrigues <joao.nrr@gmail.com>
2023-03-19 18:47:59 -0300

    create ex303

---
+++ c03/ex303.py
@@ -0,0 +1,9 @@
+# 3-3. Your Own List: Think of your favorite mode of transportation, such as a
+# motorcycle or a car, and make a list that stores several examples. Use your list
+# to print a series of statements about these items, such as “I would like to own a
+# Honda motorcycle.”
+
+vehicles = ["Harley Davidson", "CB500X", "Ferrari F40"]
+
+for i in vehicles:
+    print(f"I would like to own a {i}, but it's to expensive")
