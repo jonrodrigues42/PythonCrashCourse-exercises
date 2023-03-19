d66da4f18db50ec74b36b285751397d9d2e5f3a5
Jon Rodrigues <joao.nrr@gmail.com>
2023-03-19 18:51:40 -0300

    create ex304

---
+++ c03/ex304.py
@@ -0,0 +1,9 @@
+# 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
+# would you invite? Make a list that includes at least three people you’d like to
+# invite to dinner. Then use your list to print a message to each person, inviting
+# them to dinner.
+
+guests = ["Freddie Mercury", "James Hetfield", "Amy Winehouse"]
+
+for i in guests:
+    print(f"Dear {i}, would you like to dinner with me?")
